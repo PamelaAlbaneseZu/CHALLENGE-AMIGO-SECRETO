cb434a9db47fd3275d56b8b560bcbd99e9bb04f7
Pamela Albanese <albanese2190@gmail.com>
2025-08-10 16:43:55 -0400

    se crea l

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Declarando variables
+
+let listaAmigos = [];
+
+function actualizarListaAmigos() {
+    
+}
+
+function agregarAmigos() {
+    listaAmigos.push(document.getElementById("amigo").value);
+    alert("Tu amigo fue ingresado con exito");
+    limpiarCaja();
+    console.log(listaAmigos);
+}
+
+function sortearAmigos() {
+    
+}
+
+function limpiarCaja(){
+    document.getElementById("amigo").value = '';
+}
