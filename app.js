53bec2a83f64b1970c4d6a4d3b989a373cbe433a
Pamela Albanese <albanese2190@gmail.com>
2025-08-10 22:24:07 -0400

    Avanzando con las funciones

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 //crea lista
 let listaAmigos = [];
-
 //declarando funciones
 function actualizarListaAmigos() {
-    
+    document.querySelector
 }
-
 function agregarAmigos() {
-    listaAmigos.push(document.getElementById("amigo").value);
-    alert("Tu amigo fue ingresado con exito");
-    limpiarCaja();
+    nombre = document.getElementById("amigo").value
+    if (nombre == false) {
+        alert ('Porfavor, inserte un nombre')
+    }else {
+        listaAmigos.push(nombre);
+        alert(`${nombre} fue registrado con exito!`);
+        limpiarCaja();
+    }
     console.log(listaAmigos);
+    return;
 }
 
-function sortearAmigos() {
+function sortearAmigo() {
+    let listaSorteo = [];
+    let resultadoValido = true;
+    
+    while (resultadoValido == true){
+        let aleatorio = Math.floor(Math.random()* listaAmigos.length);
+        let amigo = Math.floor(Math.random()* listaSorteo.length);
+        listaAmigos.push(aleatorio);
+        listaSorteo.push(amigo);
+        if (aleatorio == listaAmigos.legth) {
+            resultadoValido = true
+            break;
+        }
+        for (let i = 0; i <listaAmigos.length; i++) {
+            console.log(i);
+        }
+    }
+     
+
+
+    elemento = document.querySelector('h2');
+    elemento.innerHTML = `${listaAmigos[aleatorio]}`;
+    
+
     
 }
 
